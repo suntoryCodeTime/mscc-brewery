12d322bd36df43e85066e986cf2ef4001fed9621
David Ley <dldavidley@gmail.com>
2020-08-01 17:56:36 -0700

    Spring Boot Project Initilization

---
+++ src/main/java/com/suntorycodetime/msccbrewery/MsccBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.suntorycodetime.msccbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsccBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsccBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suntorycodetime/msccbrewery/MsccBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suntorycodetime.msccbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
